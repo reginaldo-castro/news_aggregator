a364d75936fe40ecaa814ae785cc607b341f8302
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2025-06-22 11:54:05 -0400

    Configuração do cache e arquivos staticos

--- news_aggregator/settings.py
+++ news_aggregator/settings.py
@@ -124,7 +124,20 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
